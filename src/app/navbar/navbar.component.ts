ec9067b7bc131dc043760aba34c901f17fce46dd
ahmed hany <ahmed.karim524382@gmail.com>
2025-01-04 20:50:05 +0200

    fullstack

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+
+  isCollapsed = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  ngOnInit() {
+    this.checkScreenSize(); // التحقق عند تحميل الصفحة لأول مرة
+  }
+
+  private checkScreenSize() {
+    const screenWidth = window.innerWidth;
+    this.isCollapsed = screenWidth < 768; // إضافة الكلاس عند عرض الشاشة أقل من 768px
+  }
 }
